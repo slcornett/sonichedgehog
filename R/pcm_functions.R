b4272cc9a777676a45fece608aa257cc92d77d4f
slcornett <slcornett@utexas.edu>
2022-05-11 23:06:00 -0500

    adjusted AlignAA and treeAA functions, added raw git files as character vectors (Shh_fasta.f and sonic.f) so don't have to include the links in the vignette.

--- R/pcm_functions.R
+++ R/pcm_functions.R
@@ -40,7 +40,7 @@ f_file <- function(file){
 }
 
 # building a function to make a polypeptide (Amino Acid) sequence phylogenetic tree from data in fasta file ----
-#' @title pcms_AlignAA
+#' @title AlignAA
 #' @description Amino Acid multiple sequence alignment using the MUSCLE algorithm of fasta formatted data
 #' @param file a fasta file assigned to a vector in your environment
 #' @keywords multiple sequence alignment, fasta, peptide, AA alignment
@@ -48,36 +48,43 @@ f_file <- function(file){
 #' @examples using the built-in Shh_fasta data of sonic hedgehog signal molecule orthologs
 #' file <- "link to raw data on git hub"
 #' pcms_AlignAA(file) # the output of this will be the sequences aligned by the MUSCLE algorithm
-pcms_AlignAA <- function(file){
+AlignAA <- function(file){
+  class(file) == "character"
   #for reading multiple AA sequences from biostrings package
   fas <- Biostrings::readAAStringSet(file, format = "fasta", use.names = TRUE)
   # align the fasta file using MUSCLE algorithm: multiple sequence alignment from msa package
   fas_msa <- fas %>% msa::msa(method = c("Muscle"), type = "protein", order=c("aligned", "input"))
+  print(fas_msa, showConsensus = TRUE, show = "complete")
   return(fas_msa)
 }
 
 # Building a function to calculate the phylogenetic distance between AAs, then plot a tree ----
-#' @title read_file
+#' @title treeAA
 #' @description
 #' @param # one line for each parameter in our function
 #' @keywords # to aid in searching functions
 #' @export # makes the function available for others to use when your package is loaded
 #' @examples # sample code
-pcms_treeAA <- function(x){#the input to this function is the out put of the above function
-  class(x) == "MsaAAMultipleAlignment"
+treeAA <- function(x){ #the input to this function is the out put of the above function
+  class(x) == "MsaAAMultipleAlignment" # the only class these functions work on
   #read aligned data, storing in AAbin format (class will be AAbin) (ape package)
   dist_fas_msa <- x %>% ape::as.AAbin(show.aa = TRUE, check.names = TRUE) %>% # AAbin storage
-    ape::dist.aa() # distance AA
+    ape::dist.aa(pairwise.deletion = TRUE, scaled = TRUE) # distance AA
+  print(dist_fas_msa) # prints this but not the other two
   #neighbor joining method
-  tree <- ape::nj(dist_fas_msa) # new tree
-  ggt <- ggtree::ggtree(tree, cex = 1, aes(color=branch.length)) +
-    scale_color_continuous(high='green',low='blue') +
-    geom_tiplab(align = FALSE, size = 2) +
-    geom_treescale(y = 0, color = "coral4", fontsize = 4)
-  return(dist_fas_msa)
-  return(tree)
-  return(ggt)
+  tree <- ape::nj(dist_fas_msa) # new tree by neighbor joining method
+  return(tree) #  need to be able to extract this from the vector so can modify outgroup
+  ggt <- ggtree::ggtree(tree, #the new tree as data
+                        cex = 1, # branch line thickness
+                        aes(color = branch.length), # the color factor is branch length
+                        layout = "roundrect") + # style
+    scale_color_continuous(high = 'green', low = 'darkgreen') + # the colors used for indicating branch length
+    theme(legend.position = "left") + # position of the legend
+    geom_tiplab(align = FALSE, size = 3) + # tips not aligned, font size = 3
+    geom_treescale(x= 0, y = 0, color = "black", fontsize = 3) # the tree scale (number of substitutions)
+  print(ggt) # not printed
 }
+
 
 #Plot the maximum likelihood----
 #' @title read_file
